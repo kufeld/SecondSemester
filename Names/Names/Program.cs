d40accf87f5dc2d4393fc248756a5caf28a9049f
A.Kufeld <kufeldnp@gmail.com>
2020-05-25 23:13:40 +0500

    Списки. Задание 2

---
+++ Names/Names/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading;
+
+namespace Names
+{
+    class Program
+    {
+        static void Main()
+        {
+            var names = new List<string>();
+
+            Console.WriteLine("Обрабатываю данные");
+
+            try
+            {
+                using (StreamReader sr = new StreamReader("names.txt"))
+                {
+                    string line;
+                    while((line = sr.ReadLine())!= null)
+                    {
+                        Console.Write('.');
+                        names.Add(line);
+                    }
+                }
+            }
+            catch { }
+
+            names.Sort();
+
+            var result = new List<string>();
+            var currentName = names[0];
+            result.Add(currentName);
+
+            foreach (var e in names)
+            {
+                if (e != currentName)
+                    result.Add(e);
+                currentName = e;
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Обработка данных завершена");
+            Console.WriteLine($"Имен списке: {result.Count}");
+
+            try
+            {
+                using(StreamWriter sr = new StreamWriter("result.txt",false, Encoding.Default))
+                {
+                    foreach (var e in result)
+                        sr.WriteLine(Char.ToUpper(e[0]) + e.Substring(1, e.Length-1));
+                }
+            }
+            catch { }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Names/Names/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Names")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Names")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("cc1f8d11-6430-4b47-96a8-f2009d1d89b2")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
